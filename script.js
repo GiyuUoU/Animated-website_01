aa44b732c91c3ae304b5b8fc1a988ad625847570
Rudr7_7 <ankitgautam50366@gmail.com>
2025-11-26 18:22:34 +0530

    adding animation to navbar

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-var tl = gsap.timeline()
+var menu = document.querySelector("#nav i")
+var cross = document.querySelector("#full i")
+
+var tl = gsap.timeline();
+
 tl.to("#full",{
     right:0,
-    deuration:0.8,
+    duration:0.7,
 })
 
 tl.from("#full h4",{
     x:150,
     opacity:0,
     duration:0.7,
-    stagger:0.28,
+    stagger:0.20,
 })
 tl.from("#full i",{
     opacity:0,
-}
+})
+
+tl.pause()
+
+menu.addEventListener("click",()=>{
+    tl.play()
+})
+cross.addEventListener("click",()=>{
+    tl.reverse()
+})
+
+gsap.from("#nav",{
+    y:-100,
+    opacity:0,
+    duration:2,
+    ease:"easeInOut",
+})
